e4a7e412dc9dc834c8bf6ece4618dd34f837ee5a
Erik Tollerud <erik.tollerud@gmail.com>
2015-04-08 17:34:33 -0400

    add progress bar

--- emceemr/core.py
+++ emceemr/core.py
@@ -104,16 +104,38 @@ class Model(object):
         iparams = [self.get_prior(n).initialize(nwalkers) for n in self.param_names]
         return np.array(iparams).T
 
-    def initialize_and_sample(self, iters, burnin=None, nwalkers='4p', **kwargs):
+    def initialize_and_sample(self, iters, burnin=None, nwalkers='4p',
+                                    use_progress_bar=True, **kwargs):
         sampler = self.get_sampler(nwalkers=nwalkers, **kwargs)
         iparams = self.initalize_params(sampler.k)
 
         if burnin:
-            iparams = sampler.run_mcmc(iparams, burnin)[0]
+            iparams = _run_mcmc_maybe_progress_bar(sampler, iparams, burnin,
+                                                   None, None, use_progress_bar)[0]
             sampler.burnin_chain = sampler.chain
             sampler.burnin_lnprob = sampler.lnprobability
             sampler.reset()
 
-        sampler.run_mcmc(iparams, iters)
+        _run_mcmc_maybe_progress_bar(sampler, iparams, iters, None, None, use_progress_bar)
 
         return sampler
+
+def _run_mcmc_maybe_progress_bar(sampler, p0, iters, rstate0, lnprob0, use_progress_bar):
+    if use_progress_bar:
+        from astropy.utils.console import ProgressBar
+
+        try:
+            use_ipy_widget = get_ipython().config['IPKernelApp']['parent_appname'] == 'ipython-notebook'
+        except:
+            # means we're not in IPython
+            use_ipy_widget = False
+
+        with ProgressBar(iters, ipython_widget=use_ipy_widget) as bar:
+            #run_mcmc is esentially the same as this for loop, but without the update
+            for results in sampler.sample(p0, rstate0, lnprob0, iterations=iters):
+                bar.update()
+            sampler._last_run_mcmc_result = results[:3]
+    else:
+        results = sampler.run_mcmc(p0, iters, rstate0, lnprob0)
+
+    return results
