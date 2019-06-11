5e94856d65ae162b78ee63d0f076682095cbbe82
Erik Tollerud <erik.tollerud@gmail.com>
2019-06-11 13:57:33 -0400

    update to more accurate version of noise_model

--- emceemr/noise_models.py
+++ emceemr/noise_models.py
@@ -5,10 +5,16 @@ from astropy.utils import find_current_module
 
 __all__ = ['generate_logpdfmodel_class']
 
+
 def generate_logpdfmodel_class(frozen_distribution,
                                scale_param_name='prob_scale',
                                location_param_name='prob_loc',
-                               data_param_name='prob_loc'):
+                               mean_model_param_name=None):
+    """
+    mean_model_param_name = None means "whatever the location parameter is"
+    """
+    if mean_model_param_name is None:
+        mean_model_param_name = location_param_name
 
     dist = frozen_distribution.dist
     other_params, loc, scale = dist._parse_args(*frozen_distribution.args, **frozen_distribution.kwds)
@@ -23,23 +29,24 @@ def generate_logpdfmodel_class(frozen_distribution,
     params.append(modeling.Parameter(name=location_param_name, default=loc, description='distribution location parameter'))
     params.append(modeling.Parameter(name=scale_param_name, default=scale, description='distribution scale parameter'))
 
-    params = collections.OrderedDict(((p.name, p) for p in params))
-
-    members = {}
-
-    def tester(*args, **kwargs):
-        print(args, kwargs)
-        return dist.logpdf(*args, **kwargs)
+    params = collections.OrderedDict(((p.name, p) for p in params if p.name != mean_model_param_name))
 
     mod = find_current_module(2)
     modname = mod.__name__ if mod else '__main__'
 
+    def log_pdf_with_meanmodel(*args):
+        kwargs = dict(zip(params.keys(), args[2:]))
+        kwargs[mean_model_param_name] = args[1]
+        kwargs['loc'] = kwargs.pop(location_param_name)
+        kwargs['scale'] = kwargs.pop(scale_param_name)
+        return dist.logpdf(args[0], **kwargs)
+
     members = {
         '__module__': str(modname),
         'distribution': dist,
-        'inputs': ('x',),
+        'inputs': ('data', 'mean_model'),
         'outputs': ('logpdf',),
-        'evaluate': staticmethod(dist.logpdf),
+        'evaluate': staticmethod(log_pdf_with_meanmodel),
     }
     members.update(params)
 
--- emceemr/tests/test_noise_models.py
+++ emceemr/tests/test_noise_models.py
@@ -12,14 +12,23 @@ def test_logpdfmodel_generator():
     assert Mod.name == 'BetaLogPDFModel'
 
     mod = Mod()
-    assert mod(1) == stats.beta.logpdf(1, 1, 2, scale=3)
+    assert mod(1, 4) == stats.beta.logpdf(1, 1, 2, scale=3, loc=4)
     assert Mod.__module__ == find_current_module(1).__name__
 
     Mod2 = generate_logpdfmodel_class(stats.norm(scale=1), scale_param_name='sigma')
-    mod2 = Mod2(prob_loc=[0, 1])
+    mod2 = Mod2()
     mod2.sigma = 1.5
+
+    val1 = stats.norm.logpdf(0, loc=1, scale=1.5)
+    val2 = stats.norm.logpdf(1, loc=1, scale=1.5)
+    assert np.all(mod2([0, 1], [1, 1]) == np.array([val1, val2]))
+    assert np.all(mod2([0, 1], 1) == np.array([val1, val2]))
 
     val1 = stats.norm.logpdf(0, loc=0, scale=1.5)
     val2 = stats.norm.logpdf(0, loc=1, scale=1.5)
+    assert np.all(mod2([0, 0], [0, 1]) == np.array([val1, val2]))
 
-    assert np.all(mod2(0) == np.array([val1, val2]))
+    mod2_2 = Mod2(sigma=[2, 3])
+    val1 = stats.norm.logpdf(0, loc=1, scale=2)
+    val2 = stats.norm.logpdf(0, loc=1, scale=3)
+    assert np.all(mod2_2(0, 1) == np.array([val1, val2]))
